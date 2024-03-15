58e22319eed48416d58e1fcc77c8cf6b473b8ea1
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-03-16 01:33:05 +0530

    API Updated

--- backend/controllers/cabController.js
+++ backend/controllers/cabController.js
@@ -18,5 +18,21 @@ const getCabById = async (req, res) => {
   }
 };
 
+const updateCab = async (req, res) => {
+  try {
+    const { name, pricePerMinute } = req.body;
+    const cab = await Cab.findById(req.params.id);
+    if (cab) {
+      cab.name = name;
+      cab.pricePerMinute = pricePerMinute;
+      const updatedCab = await cab.save();
+      res.json(updatedCab);
+    } else {
+      res.status(404).json({ message: "Cab not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export { getCabs, getCabById };
+export { getCabs, getCabById , updateCab };
